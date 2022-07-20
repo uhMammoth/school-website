bfb23e1c12f20c466939ba1de374e1f9cdd9596b
Shaun Kiszonas <smkiszonas@gmail.com>
2022-07-19 21:14:58 -0700

    navbar/footer desktop

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -25,13 +25,13 @@ const LogIn = () => {
                     <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-900 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 </div>
                 
-                <div class="grid md:grid-cols-2 md:gap-6">
+                <div class="grid md:grid-cols-2 md:gap-6 items-center">
                     <button type="submit" class=" text-white bg-blue-800 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm w-full px-5 py-2.5 text-center  ">Submit</button>
+                    <div class="relative z-0 w-full group flex text-center">
+                    <Link className='text-md text-gray-800 ml-4 underline font-semibold'  to='/Signup'>create account</Link>
+                </div>
                 </div>
                 
-                <div class="relative z-0 mt-6 w-full group flex">
-                    <Link className='text-md text-gray-800 mt-4 underline font-semibold'  to='/Signup'>create account</Link>
-                </div>
             </form>
             </div>
             </div>
