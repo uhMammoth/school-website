df9de8b641c114f84369ca6c4980b759ff5939c8
KyleP <kyl3p3tty@gmail.com>
2022-07-22 14:29:37 -0700

    client/mutations adjustment

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,22 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
+  mutation Login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
-        _id
-        username
+        id
       }
+    }
+  }
+`;
+
+export const UPDATE_SCHED = gql`
+  mutation Schedule($counselor: Boolean!, $scheduleDays: [String]!, $scheduleTimes: [String]!) {
+    schedule(counselor: $counselor, scheduleDays: $scheduleDays, scheduleTimes: $scheduleTimes) {
+      id
+      scheduleDays
+      scheduleTimes
     }
   }
 `;
