4674594a31a4465682400314f55961240085b7b3
Capwonderbread <hectcast10@yahoo.com>
2022-07-27 12:45:38 -0700

    added a function to set the formstate and fixed DOM errors

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,36 +3,66 @@ import Bottom from '../assets/calenderbottom.jpg';
 import sidebar from '../assets/sidebar.jpg'
 import Calendar from '../components/Calendar';
 import UserAppt from '../components/UserAppt';
-import {useQuery} from '@apollo/client';
+import {useQuery, useMutation} from '@apollo/client';
 import {COUNSELORS} from '../utils/queries';
+import {ADD_APPT} from '../utils/mutations'
 
 /*  dropdown bar  */
 export default function Dashboard() {
 
-    const {data} = useQuery(COUNSELORS);
-    const counselors = data?.counselors || {};
-    console.log(counselors);
+  const {data} = useQuery(COUNSELORS);
+  const counselors = data?.counselors || {};
+  console.log(counselors);
 
-    const [showDetails, setShowDetails] = useState(false);
-    const [changeCounselor, setCounselor] = useState(false);
-    const [currentTime, setCurrentTime] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
+  const [currentTime, setCurrentTime] = useState(false);
+  const [setData] = useState(null);
 
-    const [setData] = useState(null);
+  const [formState, setFormState] = useState({ counselorId: '', date: '',  time: '', subject: ''});
+  const [addAppt, { error }] = useMutation(ADD_APPT);
 
-    if (!Array.isArray(counselors) || counselors.length <= 0) {
-      return null;
+  const [counselor, setCounselor] = useState();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  console.log(formState);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await addAppt({
+        variables: { ...formState },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+
+    setFormState({
+      counselorId: '',
+      date: '',
+      time: '',
+      subject: ''
+    });
+  };
+
+
+  if (!Array.isArray(counselors) || counselors.length <= 0) {
+    return null;
   }
 
-    const showDetailsHandle = (dayStr) => {
-      setData(dayStr);
-      setShowDetails(true);
-    };
+  const showDetailsHandle = (dayStr) => {
+    setData(dayStr);
+    setShowDetails(true);
+  };
 
-    const handleSelect = (e) => {
-      const selectedCounselor = (counselors.find(counselor => (counselor._id === e.target.value)));
-      setCounselor(selectedCounselor);
-      }
-
+  
 
   return (
 <section className="lg:grid lg:grid-cols-12 lg:gap-3">
@@ -43,8 +73,8 @@ export default function Dashboard() {
     <img src={Bottom} alt='students using laptop together' className='mt-10 md:mt-14 top-0 w-full h-40 md:h-96' /> 
     <div className='nav '>
       <div className='nav rblue h-60 lg:h-5/6 bg-slate-300'>
-        <form>
-          <select className='pb-4 w-full flex justify-center items-center bg-blue-500 text-center text-white'  onChange={handleSelect}>
+        <form onSubmit={handleFormSubmit}>
+          <select className='pb-4 w-full flex justify-center items-center bg-blue-500 text-center text-white' name='counselorId' onChange={handleChange}>
             <option value='Counselor' selected hidden>Choose Counselor</option>
             {counselors.map((counselor) => (
             <option className='bg-white text-black' key={counselor._id} value={counselor._id}>
@@ -55,14 +85,14 @@ export default function Dashboard() {
             <Calendar showDetailsHandle={showDetailsHandle} />
             <div className='bg-slate-300 h-full w-full'>
               <div className='flex flex-wrap justify-center pb-2 bg-white m-auto border-2 border-orange-500'>
-                {changeCounselor.scheduleTimes?.split(' ').map((time) => (
+                {formState.scheduleTimes?.split(' ').map((time) => (
                 <div className={`flex px-1 pt-2 bg-transparent ${currentTime === time ? 'is-active' : 'not-active'}`} key={time}>
-                <span className='cursor-pointer border-2 border-black w-fit rounded-md' onClick={() => {setCurrentTime(time);}} >{time}</span>
+                <span className='cursor-pointer border-2 border-black w-fit rounded-md' name='time' onClick={handleChange} >{time}</span>
                 </div>
                 ))}
               </div>
               <div className='flex justify-around items-center pt-10'>
-                <select className='py-1 text-center rounded-md bg-blue-500 text-white'>
+                <select className='py-1 text-center rounded-md bg-blue-500 text-white' name='subject' onChange={handleChange}>
                   <option value='subject' selected hidden>Subject</option>
                   <option value='Schedule'>Schedule Classes</option>
                   <option value='Mental Health'>Mental Health</option>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,23 +15,23 @@ const Home = () => {
   {/*  mobile  */}  
   <div className='lg:col-span-8 lg:w-[1200px]' >
   <img src={home} alt='students chilling with a dog' className=' iimage w-full mt-12 sm:h-40 md:mt-14 md:h-80 lg:h-96 shadow-lg'/>
-    <div class=" h-auto w-auto  sm:pb-4 md:mx-16">
+    <div className=" h-auto w-auto  sm:pb-4 md:mx-16">
       <div className='text-2xl py-3 rblue lg:text-4xl'>We're here for you!</div>
-      <div class="text-left mb-2 px-5 text-lg rblue lg:text-3xl">Make an Appointment Today.</div>
-      <div class="mb-2 px-5 text-sm text-left indent-6 rblue lg:text-xl">Lorem ipsum dolor sit amet consectet sit amet consectetur adipisicin sit amet consectetur adipisicinur adipisicing elitsit amet consectetur adipisicing elit. A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborum soluta, voluptate dolorem dolore saepe iusto quam mollitia</div>
-      <div class="mb-2 px-5 text-sm text-left indent-6 rblue lg:text-xl">Lorem ipsum dolor sit amet consectet sit amet consectetur adipisicin sit amet consectetur adipisicinur adipisicing elitsit amet consectetur adipisicing elit. A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborum soluta, voluptate dolorem dolore saepe iusto quam mollitia</div>
-      <div class="text-left mb-2 px-5 text-lg rblue lg:text-3xl">Services for you.</div>
-      <div class="my-2 text-left px-5 indent-6 text-sm text-blue-900 lg:text-xl">Lorem ipsum dolor sit amet consectetur adipisici sit amet consectetur  A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborsit amet consectetur adipisicing elitsit amet consectetur adipisicing elitsit amet consectetur adipisicing elitsit amet consectetur adipisicing eum soluta</div>
-      <div class="mb-2 px-5 text-sm text-left indent-6 rblue lg:text-xl">Lorem ipsum dolor sit amet consectet sit amet consectetur adipisicin sit amet consectetur adipisicinur adipisicing elitsit amet consectetur adipisicing elit. A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborum soluta, voluptate dolorem dolore saepe iusto quam mollitia</div>
+      <div className="text-left mb-2 px-5 text-lg rblue lg:text-3xl">Make an Appointment Today.</div>
+      <div className="mb-2 px-5 text-sm text-left indent-6 rblue lg:text-xl">Lorem ipsum dolor sit amet consectet sit amet consectetur adipisicin sit amet consectetur adipisicinur adipisicing elitsit amet consectetur adipisicing elit. A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborum soluta, voluptate dolorem dolore saepe iusto quam mollitia</div>
+      <div className="mb-2 px-5 text-sm text-left indent-6 rblue lg:text-xl">Lorem ipsum dolor sit amet consectet sit amet consectetur adipisicin sit amet consectetur adipisicinur adipisicing elitsit amet consectetur adipisicing elit. A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborum soluta, voluptate dolorem dolore saepe iusto quam mollitia</div>
+      <div className="text-left mb-2 px-5 text-lg rblue lg:text-3xl">Services for you.</div>
+      <div className="my-2 text-left px-5 indent-6 text-sm text-blue-900 lg:text-xl">Lorem ipsum dolor sit amet consectetur adipisici sit amet consectetur  A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborsit amet consectetur adipisicing elitsit amet consectetur adipisicing elitsit amet consectetur adipisicing elitsit amet consectetur adipisicing eum soluta</div>
+      <div className="mb-2 px-5 text-sm text-left indent-6 rblue lg:text-xl">Lorem ipsum dolor sit amet consectet sit amet consectetur adipisicin sit amet consectetur adipisicinur adipisicing elitsit amet consectetur adipisicing elit. A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborum soluta, voluptate dolorem dolore saepe iusto quam mollitia</div>
     </div>
-    <div class="nav h-auto w-auto md:px-16 p-3 mx-auto covid">
-      <div class="text-left mb-2 text-lg px-5 text-red-500 lg:text-3xl">Please note: Covid-19 restrictions</div>
-      <div class="mb-2 text-left indent-6 px-5 text-sm text-red-500 lg:text-xl">Lorem ipsum dolor sit amet consectet sit amet consectetur adipisicin sit amet consectetur adipisicinur adipisicing elitsit amet consectetur adipisicing elit. A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborum soluta, voluptate dolorem dolore saepe iusto quam mollitia Lorem ipsum dolor sit amet csicing elit.sit amesit amet consectetur adipit consectetur adipisic.</div>
+    <div className="nav h-auto w-auto md:px-16 p-3 mx-auto covid">
+      <div className="text-left mb-2 text-lg px-5 text-red-500 lg:text-3xl">Please note: Covid-19 restrictions</div>
+      <div className="mb-2 text-left indent-6 px-5 text-sm text-red-500 lg:text-xl">Lorem ipsum dolor sit amet consectet sit amet consectetur adipisicin sit amet consectetur adipisicinur adipisicing elitsit amet consectetur adipisicing elit. A odit voluptatum impedit. Debitis, voluptates dignissimos, reiciendis maxime laborum soluta, voluptate dolorem dolore saepe iusto quam mollitia Lorem ipsum dolor sit amet csicing elit.sit amesit amet consectetur adipit consectetur adipisic.</div>
     </div>
     <img src={homebottom} alt='students hugging staring at the sun' className=' lg:h-96 bottom-0 mb-1 w-full home shadow-lg md:h-80	sm:h-40'/>
     <div className=" w-full sm:8 mb-12 bottom-0 flex justify-around h-auto">  
-      <button type="button" class=" nav bgblue yellow w-full font-bold uppercase text-sm px-6 py-3  shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">Know the team</button>
-      <button type="button" class="  nav bgblue yellow w-full font-bold uppercase text-sm px-6 py-3 shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">Schedule here</button>
+      <button type="button" className=" nav bgblue yellow w-full font-bold uppercase text-sm px-6 py-3  shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">Know the team</button>
+      <button type="button" className="  nav bgblue yellow w-full font-bold uppercase text-sm px-6 py-3 shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150">Schedule here</button>
     </div>
     
     </div>
